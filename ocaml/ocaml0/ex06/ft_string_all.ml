abc63c69d20c57fa9b459e040f89cfcb750fa5d3
wyoheiii <seikimatu4649@gmail.com>
2025-10-09 23:38:45 +0900

    ex06

--- ocaml/ocaml0/ex06/ft_string_all.ml
+++ ocaml/ocaml0/ex06/ft_string_all.ml
@@ -1,6 +1,19 @@
 let is_digit c =
   c >= '0' && c <= '9'
 
-let ft_string_all pred f str =
-  let rec aux i =
-    
+let ft_string_all f str =
+  let len = String.length str in
+
+  let rec aux i len =
+    if i == len then
+      true
+    else
+      aux (i + 1) len && f (String.get str i)
+
+  in
+  aux 0 len
+
+let () = 
+  print_endline (string_of_bool (ft_string_all is_digit "0123456789"));
+  print_endline (string_of_bool (ft_string_all is_digit "01234a6789"));
+  print_endline (string_of_bool (ft_string_all is_digit ""));
